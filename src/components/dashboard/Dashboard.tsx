bc7f5a7a05a7581e3ec9694addbd7827dfd65e5c
wangxi <xib1102@icloud.com>
2019-12-14 20:17:23 +0800

    finish 60%

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -22,7 +22,7 @@ class Dashboard extends React.Component {
                                         <Icon type="heart" className="text-2x text-danger" />
                                     </div>
                                     <div className="clear">
-                                        <div className="text-muted">收藏</div>
+                                        <div className="text-muted">出票金额</div>
                                         <h2>301</h2>
                                     </div>
                                 </div>
@@ -35,7 +35,7 @@ class Dashboard extends React.Component {
                                         <Icon type="cloud" className="text-2x" />
                                     </div>
                                     <div className="clear">
-                                        <div className="text-muted">云数据</div>
+                                        <div className="text-muted">今日盈利</div>
                                         <h2>30122</h2>
                                     </div>
                                 </div>
@@ -50,7 +50,7 @@ class Dashboard extends React.Component {
                                         <Icon type="camera" className="text-2x text-info" />
                                     </div>
                                     <div className="clear">
-                                        <div className="text-muted">照片</div>
+                                        <div className="text-muted">今日订单数</div>
                                         <h2>802</h2>
                                     </div>
                                 </div>
@@ -63,104 +63,38 @@ class Dashboard extends React.Component {
                                         <Icon type="mail" className="text-2x text-success" />
                                     </div>
                                     <div className="clear">
-                                        <div className="text-muted">邮件</div>
+                                        <div className="text-muted">总订单数</div>
                                         <h2>102</h2>
                                     </div>
                                 </div>
                             </Card>
                         </div>
                     </Col>
-                    <Col className="gutter-row" md={16}>
-                        <div className="gutter-box">
-                            <Card bordered={false} className={'no-padding'}>
-                                <EchartsProjects />
-                            </Card>
-                        </div>
-                    </Col>
-                </Row>
-                <Row gutter={10}>
-                    <Col className="gutter-row" md={8}>
+                    <Col className="gutter-row" md={4}>
                         <div className="gutter-box">
                             <Card bordered={false}>
-                                <div className="pb-m">
-                                    <h3>任务</h3>
-                                    <small>10个已经完成，2个待完成，1个正在进行中</small>
+                                <div className="clear y-center">
+                                    <div className="pull-left mr-m">
+                                        <Icon type="camera" className="text-2x text-info" />
+                                    </div>
+                                    <div className="clear">
+                                        <div className="text-muted">中奖订单数</div>
+                                        <h2>802</h2>
+                                    </div>
                                 </div>
-                                <span className="card-tool"><Icon type="sync" /></span>
-                                <Timeline>
-                                    <Timeline.Item color="green">新版本迭代会</Timeline.Item>
-                                    <Timeline.Item color="green">完成网站设计初版</Timeline.Item>
-                                    <Timeline.Item color="red">
-                                        <p>联调接口</p>
-                                        <p>功能验收</p>
-                                    </Timeline.Item>
-
-                                    <Timeline.Item color="#108ee9">
-                                        <p>登录功能设计</p>
-                                        <p>权限验证</p>
-                                        <p>页面排版</p>
-                                    </Timeline.Item>
-                                </Timeline>
                             </Card>
                         </div>
-                    </Col>
-                    <Col className="gutter-row" md={8}>
                         <div className="gutter-box">
                             <Card bordered={false}>
-                                <div className="pb-m">
-                                    <h3>消息栏</h3>
+                                <div className="clear y-center">
+                                    <div className="pull-left mr-m">
+                                        <Icon type="mail" className="text-2x text-success" />
+                                    </div>
+                                    <div className="clear">
+                                        <div className="text-muted">客户数</div>
+                                        <h2>102</h2>
+                                    </div>
                                 </div>
-                                <span className="card-tool"><Icon type="sync" /></span>
-                                <ul className="list-group no-border">
-                                    <li className="list-group-item">
-                                        <span className="pull-left w-40 mr-m">
-                                            <img src={b1} className="img-responsive img-circle" alt="test" />
-                                        </span>
-                                        <div className="clear">
-                                            <span className="block">鸣人</span>
-                                            <span className="text-muted">终于当上火影了！</span>
-                                        </div>
-                                    </li>
-                                    <li className="list-group-item">
-                                        <span className="pull-left w-40 mr-m">
-                                            <img src={b1} className="img-responsive img-circle" alt="test" />
-                                        </span>
-                                        <div className="clear">
-                                            <span className="block">佐助</span>
-                                            <span className="text-muted">吊车尾~~</span>
-                                        </div>
-                                    </li>
-                                    <li className="list-group-item">
-                                        <span className="pull-left w-40 mr-m">
-                                            <img src={b1} className="img-responsive img-circle" alt="test" />
-                                        </span>
-                                        <div className="clear">
-                                            <span className="block">小樱</span>
-                                            <span className="text-muted">佐助，你好帅！</span>
-                                        </div>
-                                    </li>
-                                    <li className="list-group-item">
-                                        <span className="pull-left w-40 mr-m">
-                                            <img src={b1} className="img-responsive img-circle" alt="test" />
-                                        </span>
-                                        <div className="clear">
-                                            <span className="block">雏田</span>
-                                            <span className="text-muted">鸣人君。。。那个。。。我。。喜欢你..</span>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </Card>
-                        </div>
-                    </Col>
-                    <Col className="gutter-row" md={8}>
-                        <div className="gutter-box">
-                            <Card bordered={false}>
-                                <div className="pb-m">
-                                    <h3>访问量统计</h3>
-                                    <small>最近7天用户访问量</small>
-                                </div>
-                                <span className="card-tool"><Icon type="sync" /></span>
-                                <EchartsViews />
                             </Card>
                         </div>
                     </Col>
